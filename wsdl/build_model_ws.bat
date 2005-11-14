6db5ed0057857324b2ae139eb8dfc0d4bf0086d2
smarkle <smarkle>
2005-11-14 21:04:12 +0000

    Migration towards AWS.

---
+++ wsdl/build_model_ws.bat
@@ -0,0 +1,1 @@
+java -cp ..\lib\axis-ant.jar;..\lib\axis.jar;..\lib\commons-discovery.jar;..\lib\commons-logging.jar;..\lib\jaxrpc.jar;..\log4j-1.2.8.jar;..\lib\saaj.jar;..\lib\wsdl4j.jar org.apache.axis.wsdl.WSDL2Java -v -W -o ..\src -p com.amazon.merchants.transport.wsclient AWSMerchantService.wsdl
