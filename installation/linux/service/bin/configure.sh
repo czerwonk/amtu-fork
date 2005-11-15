73f05773394a327a9a6be6903aa2ff18b2769cf6
smarkle <smarkle>
2005-11-15 21:30:55 +0000

    Moving from galega to amtu naming.

--- installation/linux/service/bin/configure.sh
+++ installation/linux/service/bin/configure.sh
@@ -1,3 +1,3 @@
-CLASSPATH=../../lib/axis.jar:../../lib/commons-discovery.jar:../../lib/commons-lang-2.0.jar:../../lib/commons-logging.jar:../../lib/galega.jar:../../lib/jaxrpc.jar:../../lib/activation.jar:../../lib/mailapi.jar:../../lib/saaj.jar:../../lib/smtp.jar:../../lib/wsdl4j.jar:../../lib/xercesImpl.jar:../../lib/xml-apis.jar
+CLASSPATH=../../lib/axis.jar:../../lib/commons-discovery.jar:../../lib/commons-lang-2.0.jar:../../lib/commons-logging.jar:../../lib/amtu.jar:../../lib/jaxrpc.jar:../../lib/activation.jar:../../lib/mailapi.jar:../../lib/saaj.jar:../../lib/smtp.jar:../../lib/wsdl4j.jar:../../lib/xercesImpl.jar:../../lib/xml-apis.jar
 
 $JAVA_HOME/bin/java -cp $CLASSPATH com.amazon.merchants.transport.preferences.Configure $1
