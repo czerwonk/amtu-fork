234bc7d3b1fcac64e9bbf4d88fabcb6464610aea
smarkle <smarkle>
2005-11-25 22:27:27 +0000

    *** empty log message ***

--- dev_tools/LaunchClient.bat
+++ dev_tools/LaunchClient.bat
@@ -1,2 +1,3 @@
-set CLASSPATH=lib\mx4j-remote.jar:lib\mx4j-rimpl.jar:lib\mx4j-rjmx.jar:lib\amtu.jar:lib\mx4j.jar:lib\mx4j-impl.jar:lib\mx4j-jmx.jar
+set CLASSPATH=lib\mx4j-remote.jar;lib\mx4j-rimpl.jar;lib\mx4j-rjmx.jar;lib\amtu.jar;lib\mx4j.jar;lib\mx4j-impl.jar;lib\mx4j-jmx.jar
+
 jre\bin\java -cp %CLASSPATH% com.amazon.merchants.remote.Client
