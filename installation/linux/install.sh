67b8de28358a887237646af7acb8bc7beb1feb35
pengzhai <pengzhai>
2007-03-05 23:43:46 +0000

    (1) Check if $JAVA_HOME has been set. (2) Changed hard-coded link to java binary to use system path.

--- installation/linux/install.sh
+++ installation/linux/install.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
 
-export JAVA_HOME=/opt/third-party/depot/Linux-2.4c2.2-i686/java-1.4.2_06_SUN
+if [ -z "$JAVA_HOME" ]; then
+  echo "The JAVA_HOME environment variable is not defined"
+  echo "This environment variable is needed to run this program"
+  exit 1
+fi
+echo "JAVA_HOME: " $JAVA_HOME
 export PATH=$JAVA_HOME/bin:$PATH
 export AMTU_HOME=`pwd`
+echo "AMTU_HOME: " $AMTU_HOME
 
 ./configure.sh
 cd service/bin
